ba2a0091e233f8ef2364408082f492f78471d6aa
Glauber Costa <glauber@chiselstrike.com>
2023-01-20 18:09:43 -0500

    adding a beta tap

---
+++ Formula/sqld-beta.rb
@@ -0,0 +1,35 @@
+class SqldBeta < Formula
+  desc "SQLd is the server-mode binary for libSQL, an Open Contribution fork of SQLite"
+  homepage "https://libsql.org"
+  # pull from git tag to get submodules
+  url "https://github.com/libsql/sqld.git",
+      tag:      "v0.0.1"
+  sha256 "9f659f57f02d601b3bdc8e20f40052646403f54f1d08c475cfda97ce82f09d09"
+  license "MIT"
+
+  depends_on "rust" => :build
+  depends_on "protobuf"
+
+  def install
+    system "git", "submodule", "update", "--init", "--force", "--recursive", "--depth", "1"
+    cd "libsql" do
+        system "./configure"
+        system "make"
+    end
+    ENV.append "SQLITE3_STATIC", "1"
+    ENV.append "SQLITE3_LIB_DIR", "#{pwd}/libsql/.libs"
+    ENV.append "SQLITE3_INCLUDE_DIR", "#{pwd}/libsql/"
+    cd "sqld" do
+        system "cargo", "install", *std_cargo_args
+    end
+  end
+
+  test do
+      server_port = free_port
+      server_pid = fork { exec bin/"sqld", "--http-listen-addr", "127.0.0.1:#{server_port}" }
+      output = shell_output "curl --retry 5 -s -d '{\"statements\": [\"SELECT * from notable;\"] }' 127.0.0.1:#{server_port}"
+      assert_match "no such table", output 
+  ensure
+    Process.kill "SIGTERM", server_pid
+  end
+end
